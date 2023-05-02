ad68f4cd8c3bd119d017dde00697de4aa4c98e52
sueliao <c6727c@gmail.com>
2023-05-02 20:41:15 +0800

    Update format.

--- eight_queen.cs
+++ eight_queen.cs
@@ -35,44 +35,44 @@ public class Program
 		//Console.Write(string.Join(Environment.NewLine, result));
 	}
 	
-	 private static IEnumerable<int[]> Queens(int n) {
-      if (n <= 0)
-        yield break;
-      
-      int[] position = new int[n];
+	 private static IEnumerable<int[]> Queens(int n) 
+	 {
+		 if (n <= 0)
+			 yield break;
+		 int[] position = new int[n];
 
-      if (n == 1) {
-        yield return position;
-        yield break;
-      }
+		 if (n == 1) {
+			 yield return position;
+			 yield break;
+		 }
 
-      while (true) {
-        for (int file = 1; file < n; ) {
-          int rank = position[file];
+		 while (true) {
+			 for (int file = 1; file < n; ) {
+				 int rank = position[file];
+				 
+				 if (position.Take(file).Select((r, c) => r == rank ||
+								rank - r == file - c ||
+								rank - r == c - file).Any(x => x)) 
+				 {
+					 while (position[file] >= n - 1) {
+						 position[file] = 0;
+						 if (--file < 0)
+							 yield break;
+					 }
+					 
+					 position[file] += 1;
+				 }
+				 else 
+					 file += 1;
+			 }
 
-          if (position.Take(file).Select((r, c) => r == rank || 
-                rank - r == file - c || 
-                rank - r == c - file).Any(x => x)) {
-            while (position[file] >= n - 1) {
-              position[file] = 0;
-
-              if (--file < 0)
-                yield break;
-            }
-
-            position[file] += 1;
-          }
-          else 
-            file += 1;
-        }
-
-        yield return position.ToArray();
-
-        for (int i = n - 1; i >= 0; --i)
-          if (++position[i] >= n)
-            position[i] = 0;
-          else
-            break;
-      }
-    }
+			 yield return position.ToArray();
+			 
+			 for (int i = n - 1; i >= 0; --i)
+				 if (++position[i] >= n)
+					 position[i] = 0;
+			 	 else
+				 	 break;
+		 }
+	 }
 }
